5f546a703fd997aa31aa214cb1f9c5b97707841d
miguelcouteirorodrigues <miguels.rodrigues@live.com.pt>
2018-07-09 11:36:05 +0100

    Music app completed

---
+++ app/src/main/java/pt/ipvc/miguels/cmem2/MusicController.java
@@ -0,0 +1,14 @@
+package pt.ipvc.miguels.cmem2;
+
+import android.content.Context;
+import android.widget.MediaController;
+
+public class MusicController extends MediaController {
+
+    public MusicController(Context c){
+        super(c);
+    }
+
+    public void hide(){}
+
+}
